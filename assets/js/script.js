640d77eb01c7fe5550f6a1cd6a424e80c49e8867
Timi <timiplayskeys@gmail.com>
2022-07-31 22:09:28 -0400

    added detailed comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
+ 
+//  To set the currentDay
  $('#currentDay').text(moment().format('dddd MMMM Do'));
+
+ //Array of working hours
  const workHours = [9,10,11,12,13,14,15,16,17];
+
+ //To get the current time
  let currentHour = moment().format('ha');
-//  moment().format('ha');
 
+
+// to set Colors dynamically
 let timeColor;
-let timeHr;
-let textStatus
-let scheduleText
-let btnStatus
-let otherHours
-let exactTime
 
+// to set past appointments to read only 
+let textStatus
+
+//The inner text for each schedule
+let scheduleText
+
+//To disable button for past appointments so they cannot be overwritten
+let btnStatus
+
+//To get other working hours that are not the current hour
+let otherHours
+
+
+//Iterating over the working hours array
  for(let i =0; i < workHours.length; i++){
      
      otherHours = moment().hours(workHours[i ]).format('ha')
 
+     //To check if appointments are in the past and set them dynamically
     if(moment(otherHours, 'ha').isBefore(moment(currentHour,'ha'))){
         timeColor = 'past'
         scheduleText = localStorage.getItem(moment().hours(workHours[i]).format('ha')) || ''
@@ -22,17 +38,21 @@ let exactTime
         btnStatus = 'disabled'
     }
   
+    //To set the currenthour styles and text
     else if(otherHours === currentHour){
         timeColor = 'present'
         scheduleText = localStorage.getItem(moment().hours(currentHour).format('ha')) || ''
         textStatus = ''
         btnStatus = ''
     }
+
+    //To set future styles and text
     else{
         timeColor = 'future'
         scheduleText = localStorage.getItem(moment().hours(workHours[i]).format('ha')) || ''
     }
 
+    //To select the container and create a row for each element
     $('.container').append(`
     <div class="row my-1">
     <p class="col-2 col-md-1 hour">
@@ -43,11 +63,12 @@ let exactTime
   </div> </div>`)
  }
 
-
- 
+ // variable to store selected textarea values
 let textAreaText
+ // variable to time of the selected textarea values
 let timeSelected
 
+//To listen for click events on the save btn and save text to the local storage
 $('.container').on('click','.saveBtn', function(){
     textAreaText = $(this).siblings('textarea').val().trim();
     timeSelected = $(this).siblings('p').text().trim();
